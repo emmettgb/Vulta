70c393b6cfa29294352f54b8ebce2c215ec36284
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-05-15 05:59:57 -0400

    added fullscreen support

--- src/lib_VULTA/vultabase.jl
+++ src/lib_VULTA/vultabase.jl
@@ -1,7 +1,7 @@
-function VultaInstance(w::Int64,h::Int64,title::String)
+function VultaInstance(w::Int64,h::Int64,title::String,fs)
     # vk = VKInstance()
     #  ^^ Temporary until I can figure out this out-dated documentation issue.
     vk = 5
-    window = Window(w, h, title)
-    () -> (window;vk)
+    process = Window(w, h, title, fs)
+    () -> (process;vk)
 end
