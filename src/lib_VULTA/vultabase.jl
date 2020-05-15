ef302c92c2f4b992bdebf3fa5df22ad8782885c9
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-05-15 05:46:00 -0400

    temporarily commented out vkinstance

--- src/lib_VULTA/vultabase.jl
+++ src/lib_VULTA/vultabase.jl
@@ -1,5 +1,7 @@
 function VultaInstance(w::Int64,h::Int64,title::String)
-    vk = VKInstance()
+    # vk = VKInstance()
+    #  ^^ Temporary until I can figure out this out-dated documentation issue.
+    vk = 5
     window = Window(w, h, title)
     () -> (window;vk)
 end
