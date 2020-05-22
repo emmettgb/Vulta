5acc486fb8d4de2f37a5ae0cc94e276e95036df1
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-05-22 10:55:51 -0400

    Added main loop process to VULTA Instance

--- src/lib_VULTA/vultabase.jl
+++ src/lib_VULTA/vultabase.jl
@@ -1,3 +1,5 @@
+using Base.Threads: @spawn
+include("vutil.jl")
 @doc """Vulta Instance\n
 Creating a new Vulta Instance will create a new GLFW instance and Window, as
 well as a new Vulkan instance paired with it.\n
@@ -8,11 +10,28 @@ w:: The desired Width of the GLFW Window.\n
 h:: The desired height of the GLFW Window.\n
 title:: The desired title of the GLFW Window\n
 fs:: Boolean determining whether or not the window should be fullscreen.\n"""
-
 function VultaInstance(w::Int64,h::Int64,title::String,fs)
     # vk = VKInstance()
     #  ^^ Temporary until I can figure out this out-dated documentation issue.
     vk = 5
-    process = Window(w, h, title, fs)
-    () -> (process;vk)
+    p = process()
+    dltapc = proc(p)
+    get_delta() = p.delta
+    kill_dlta() = dltapc.kill_proc()
+    win = Window(w, h, title, fs)
+    terminate() = win.terminate()
+    () -> (vk;terminate;p;dltapc;get_delta;kill_dlta)
+end
+function process()
+    delta = 0
+    advance_process() = delta += 1
+    (var)->(delta;advance_process)
+end
+function proc(p)
+    process = 0
+   @spawn while process == 0
+        p.advance_process()
+    end
+    kill_proc() = process += 1
+    (var)->(kill_proc;process)
 end
